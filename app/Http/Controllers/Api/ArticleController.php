63b7cebb4615f60f34840379fb3be15717f98537
zanjs <root@zanjs.com>
2016-12-12 18:38:57 +0800

    There is a great different between exposure of the mind and that of the body :ram:

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -25,11 +25,20 @@ class ArticleController extends Controller
       if(!$article){
           return response()->json(['success' => false]);
       }
-  
+      $articleTable = 'articles';
+      $articleTagsTable = 'article_tags';
      
     //    $tags = DB::table('article_tags')->where('tag_id', 'in', 'select tag_id from article_tags where article_id=1')->get();
-       $tags = DB::select("SELECT article_id from article_tags where tag_id in (select tag_id from article_tags where article_id=".$id.") and article_id<>".$id."");
+    //    $data = DB::select("SELECT article_id from article_tags where tag_id in (select tag_id from article_tags where article_id=".$id.") and article_id<>".$id." limit 5 ");
+    //    $data = DB::select("SELECT * from articles WHERE id in(SELECT DISTINCT(article_id) from article_tags where tag_id in (select tag_id from article_tags where article_id=".$id.") and article_id<>".$id.") limit 5 ");
+       $data = DB::select("SELECT * from ".$articleTable." a RIGHT JOIN (
+           SELECT article_id,COUNT(article_id) num from ".$articleTagsTable." ats
+           where tag_id in (select tag_id from ".$articleTagsTable." where article_id=".$id.") and article_id<>".$id."
+           group by article_id order by num desc limit 5
+           ) tt
+           on a.id=tt.article_id");
+           
 
-       return response()->json(['data' => $tags]);
+       return response()->json(['success' => true,'data' => $data]);
     }
 }
